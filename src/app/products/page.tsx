4f2202ff3143b735d96a26c04fda61136c631f93
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2025-03-28 08:38:26 +0300

    feat[Products]: added work with usePathname

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,14 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const Products = () => {
   const { push } = useRouter();
+  const pathName = usePathname();
+
   return (
     <div>
       <div>Products</div>
+      <div>Текущая страница ${pathName}</div>
       <button onClick={() => push("/")}>Redirect to Home page</button>
     </div>
   );
